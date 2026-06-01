e320a0ef5ff24da20e6c7003fbd2e2bf2af499b5
Wakcedon <vaksedon@gmail.com>
2026-06-01 18:02:12 +0600

    feat: qualify ChatTabs init call in Fabric entrypoint
    - Updated `FabricEntrypoint` to use the fully qualified `net.wakcedon.chattabsreloaded.ChatTabs.init()` instead of the ambiguous import.
    - Added a comment clarifying the purpose of the initialization.
    - Fixed misplaced braces and formatting issues to ensure proper class structure.

--- src/main/java/net/wakcedon/chattabsreloaded/fabric/FabricEntrypoint.java
+++ src/main/java/net/wakcedon/chattabsreloaded/fabric/FabricEntrypoint.java
@@ -2,11 +2,12 @@ package net.wakcedon.chattabsreloaded.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.wakcedon.chattabsreloaded.ChatTabs;
-
 public class FabricEntrypoint implements ModInitializer {
-
     @Override
     public void onInitialize() {
-        ChatTabs.init();
+        // Initialize common mod entry point
+        net.wakcedon.chattabsreloaded.ChatTabs.init();
+    }
+}
     }
 }
